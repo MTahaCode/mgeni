dbe7d99a7d6763a0dc7cb3d67b4f6cb9bb7aba7b
festus-sulumeti <sulumetifestus@outlook.com>
2025-12-24 14:17:16 +0300

    chore: complete forgot-password

--- src/Auth/ForgotPassword.tsx
+++ src/Auth/ForgotPassword.tsx
@@ -1,9 +1,57 @@
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Link } from 'react-router-dom'
 
-function ForgotPassword() {
-  return (
-    <div>ForgotPassword</div>
-  )
+export default function ForgotPasswordPage() {
+    return (
+        <section className="flex min-h-screen bg-white px-4 py-16 md:py-32 dark:bg-transparent">
+            <form
+                action=""
+                className="bg-muted m-auto h-fit w-full max-w-sm overflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:[--color-muted:var(--color-zinc-900)]">
+                <div className="bg-card -m-px rounded-[calc(var(--radius)+.125rem)] border p-8 pb-6">
+                    <div className="text-center">
+                        <h1 className="mb-1 mt-4 text-xl font-semibold text-blue-600">Recover Password</h1>
+                        <p className="text-sm">Enter your email to receive a reset link</p>
+                    </div>
+
+                    <div className="mt-6 space-y-6">
+                        <div className="space-y-2">
+                            <Label
+                                htmlFor="email"
+                                className="block text-sm">
+                                Email
+                            </Label>
+                            <Input
+                                type="email"
+                                required
+                                name="email"
+                                id="email"
+                                placeholder="kamau@gmail.com"
+                            />
+                        </div>
+
+                        <Button className="w-full bg-[#ff0] text-black">Send Reset Link</Button>
+                    </div>
+
+                    <div className="mt-6 text-center">
+                        <p className="text-muted-foreground text-sm">We'll send you a link to reset your password.</p>
+                    </div>
+                </div>
+
+                <div className="p-3">
+                    <p className="text-accent-foreground text-center text-sm">
+                        Remembered your password?
+                        <Button
+                            asChild
+                            variant="link"
+                            className="px-2 text-blue-600">
+                            <Link to="/signin">Log in</Link>
+                        </Button>
+                    </p>
+                </div>
+            </form>
+        </section>
+    )
 }
-
-export default ForgotPassword
