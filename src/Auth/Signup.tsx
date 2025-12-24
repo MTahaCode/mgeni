c930e57730178d9702c36221166940959d568bf0
festus-sulumeti <sulumetifestus@outlook.com>
2025-12-24 14:03:09 +0300

    feat: import sign up image

--- src/Auth/Signup.tsx
+++ src/Auth/Signup.tsx
@@ -1,6 +1,7 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
 import { SignupForm } from "@/components/signup-form"
+import image1 from "@/assets/signup.jpeg"
 
 export default function SignupPage() {
   return (
@@ -22,7 +23,7 @@ export default function SignupPage() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.svg"
+          src={image1}
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
